e573c65a4d136d4a9ec5edd75d9dfc8953726f3e
TommasoBernardo <tommasob1210@gmail.com>
2022-11-25 16:36:13 +0100

    finish exercise

---
+++ snack-c/script.js
@@ -0,0 +1,15 @@
+//Calcola la somma dei primi 10 numeri di un array.
+
+
+const numbers = [1 , 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 8, 34, 89,5];
+
+let sum = 0;
+
+for(let i = 0 ; i < 10 ; i++){
+    sum += numbers[i];
+}
+
+console.log(sum);
+
+
+
