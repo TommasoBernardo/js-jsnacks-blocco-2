c0d4422ff36101256d1ff4318790d1f234068523
TommasoBernardo <tommasob1210@gmail.com>
2022-11-25 15:44:28 +0100

    finish exercise

---
+++ snack-a/script.js
@@ -0,0 +1,20 @@
+/*
+    Crea un array vuoto.
+    Chiedi per 6 volte all'utente di inserire un numero,
+    se è dispari inseriscilo nell'array.
+*/
+
+const number = [];
+
+for(let i = 0 ; i < 6 ; i++){
+    let user = prompt('inserisci un numero');
+
+    if((user % 2) === 0){
+        console.log('il numero è pari');
+    }else{
+        number.push(user);
+        console.log('il numero è dispari');
+    }
+}
+
+console.log(number);
